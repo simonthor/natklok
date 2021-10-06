fb50793e6777253862bfca62e4eeb281be9bcb9a
Karl Sellergren <karlsellergren@protonmail.com>
2021-10-07 01:23:52 +0200

    Added placeholder code and started working on onHover link effect

--- src/components/dynamicQuestions/SearchResult.jsx
+++ src/components/dynamicQuestions/SearchResult.jsx
@@ -4,11 +4,12 @@ import { useState } from "react";
 import { Hidden } from "@material-ui/core";
 import { WarningOutlined } from "@material-ui/icons";
 
-const SearchResults = ({ options, onSelectAnswer, t }) => {
+const SearchResults = ({ questionData, onSelectAnswer, t }) => {
   const [scams, setScams] = useState({
     httpFound: false,
     finalDomainScam: false,
   });
+  const [linkHover, setLinkHover] = useState([false,false,false,false])
 
   let amountFound = 0;
   Object.values(scams).forEach((found) => {
@@ -25,10 +26,165 @@ const SearchResults = ({ options, onSelectAnswer, t }) => {
   return (
     <div style={{
       width: "100%",
-      backgroundColor: dark ? "#151e4a" : "#e5e5ea",
+      backgroundColor: dark ? "#1d2342" : "#dedee0",
       color: dark ? "#e5e5ea" : "#151e4a",
-      padding: 20
     }}>
+      <div style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 10,
+        backgroundColor: dark ? "#10142b" : "#fff",
+      }}>
+        <div>logo</div>
+        <div style={{
+          width: "80%",
+          display: "flex",
+          height: 38,
+          alignItems: "center",
+          backgroundColor: dark ? "#212959" : "#fff",
+          padding: "0 15px",
+          borderRadius: 19,
+          fontSize: "0.85em"
+        }}>Vad kostar ett frimärke?</div>
+      </div>
+      <div style={{
+        display: "flex",
+        paddingLeft: 20,
+        alignItems: "center",
+        fontSize: "0.75em",
+        fontWeight: "bold",
+        backgroundColor: dark ? "#10142b" : "#fff",
+      }}>
+        <div style={{
+          color: "#7d9efa",
+          padding: "7px 0",
+          margin: "0 5px",
+          borderBottom: "3px solid #7d9efa"
+        }}>Allt</div>
+        <div style={{
+          padding: "7px 0",
+          margin: "0 5px",
+          borderBottom: "3px solid transparent"
+        }}>Bilder</div>
+        <div style={{
+          padding: "7px 0",
+          margin: "0 5px",
+          borderBottom: "3px solid transparent"
+        }}>Nyheter</div>
+      </div>
+      <div style={{
+        margin: 10,
+        padding: 10,
+        paddingBottom: 5
+      }}
+      onMouseEnter={() => setLinkHover([true,false,false,false])}
+      onMouseLeave={() => setLinkHover(false)}>
+        <div style={{
+          display: "flex",
+        }}>
+          <span style={{
+            fontSize: "0.6em",
+            padding: "1px 5px",
+            color: "#1d2342",
+            fontWeight: "bold",
+            borderRadius: 4,
+            backgroundColor: "#f1c232",
+            marginRight: 6
+          }}>ANNONS</span>
+          <span style={{
+          display: "block",
+          fontSize: "0.7em",
+          opacity: 0.9
+        }}>{questionData.linkSets[0].adLinks[0].url}</span>
+        </div>
+        <span style={{
+          margin: "3px 0",
+          display: "block"
+        }}>{questionData.linkSets[0].adLinks[0].title}</span>
+        <p style={{
+          margin: 0,
+          fontSize: "0.8em"
+        }}>{questionData.linkSets[0].adLinks[0].metaDesc}</p>
+      </div>
+      <div style={{
+        margin: 10,
+        padding: 10,
+        paddingBottom: 5
+      }}>
+        <div style={{
+          display: "flex",
+        }}>
+          <span style={{
+            fontSize: "0.6em",
+            padding: "1px 5px",
+            color: "#1d2342",
+            fontWeight: "bold",
+            borderRadius: 4,
+            backgroundColor: "#f1c232",
+            marginRight: 6
+          }}>ANNONS</span>
+          <span style={{
+          display: "block",
+          fontSize: "0.7em",
+          opacity: 0.9
+        }}>{questionData.linkSets[0].adLinks[1].url}</span>
+        </div>
+        <span style={{
+          margin: "3px 0",
+          display: "block"
+        }}>{questionData.linkSets[0].adLinks[1].title}</span>
+        <p style={{
+          margin: 0,
+          fontSize: "0.8em"
+        }}>{questionData.linkSets[0].adLinks[1].metaDesc}</p>
+      </div>
+      <div style={{
+        margin: 10,
+        padding: 10,
+        paddingBottom: 5
+      }}>
+        <div style={{
+          display: "flex",
+        }}>
+          <span style={{
+          display: "block",
+          fontSize: "0.7em",
+          opacity: 0.9
+        }}>{questionData.linkSets[0].suspiciousLink.url}</span>
+        </div>
+        <span style={{
+          margin: "3px 0",
+          display: "block"
+        }}>{questionData.linkSets[0].suspiciousLink.title}</span>
+        <p style={{
+          margin: 0,
+          fontSize: "0.8em"
+        }}>{questionData.linkSets[0].suspiciousLink.metaDesc}</p>
+      </div>
+      <div style={{
+        margin: 10,
+        padding: 10,
+        paddingBottom: 5
+      }}>
+        <div style={{
+          display: "flex",
+        }}>
+          <span style={{
+          display: "block",
+          fontSize: "0.7em",
+          opacity: 0.9
+        }}>{questionData.linkSets[0].legitLink.url}</span>
+        </div>
+        <span style={{
+          margin: "3px 0",
+          display: "block"
+        }}>{questionData.linkSets[0].legitLink.title}</span>
+        <p style={{
+          margin: 0,
+          fontSize: "0.8em"
+        }}>{questionData.linkSets[0].legitLink.metaDesc}</p>
+      </div>
     </div>
   );
 };
