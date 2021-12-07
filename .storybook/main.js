fc15e82cd0590f4383f84e1005a7013892baabdd
Karl Sellergren <68159964+sakerhetspolisen@users.noreply.github.com>
2021-12-07 14:20:31 +0100

    Upgraded storybook, added a11y and dark-mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "storybook-dark-mode",
     'storybook-react-i18next',
   ],
 };
