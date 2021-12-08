f6def45a147d387fec76528b46c170ae3468744b
Karl Sellergren <68159964+sakerhetspolisen@users.noreply.github.com>
2021-12-08 11:07:13 +0100

    Changed Modal story default state

--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -4,21 +4,27 @@ import Modal from "components/Modal";
 export default {
 	component: Modal,
 	title: "Components/Modal",
-
 };
 
 const Template = (args) => {
-    const [open, setOpen] = useState(false);
-    return (
-        <div style={{ color: "black"}}>
-            <Modal open={open} setOpen={setOpen} {...args} />
-        </div>
-    )
+	const [open, setOpen] = useState(false);
+	return (
+		<div style={{ color: "black" }}>
+			<Modal open={open} setOpen={setOpen} {...args} />
+		</div>
+	);
 };
 
 export const Default = Template.bind({});
 Default.args = {
-	title: "Läs mer om romansbedrägerier",
-	children: "<h1>Hej</h1>",
-    open: true
+	title: "Surfa online",
+	children: (
+		<div style={{ padding: "0 20px" }}>
+			<p>
+				Vad sitter i mitten av world wide web? En väldigt, väldigt stor
+				spindel!
+			</p>
+		</div>
+	),
+	open: true,
 };
